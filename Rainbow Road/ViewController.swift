347e793ce1d9954b713eb76a3270969f73a8f14f
Safa Falaqi <sfalaqi@gmail.com>
2021-12-08 01:17:19 +0300

    Initial Commit

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewTable: UITableView!
+    let backgroundColor:[UIColor] = [.red, .orange, .yellow,.green, .blue,.purple]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewTable.dataSource = self
+        
     }
 
 
 }
+
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return backgroundColor.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RainbowCell", for: indexPath)
+
+        cell.backgroundColor = backgroundColor[indexPath.row]
+   
+        return cell
+    }
+    
+}
+    
 
